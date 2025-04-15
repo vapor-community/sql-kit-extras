6d5930ee5451d8c768c904fbcda25009eec90aea
Paul Toffoloni <paultoffoloni@gmail.com>
2025-04-15 10:49:53 +0200

    Add `SQLLAGExpression`

---
+++ Sources/SQLKitExtras/FluentSQLKitExtras/Expressions/SQLLAG+FluentKeypaths.swift
@@ -0,0 +1,33 @@
+#if FluentSQLKitExtras
+import FluentKit
+import SQLKit
+
+extension SQLExpression {
+    /// Allow specifying an argument to the `lag()` SQL function using a Fluent model keypath.
+    static func lag(
+        _ keypath: KeyPath<some Schema, some QueryAddressableProperty>,
+        offset: KeyPath<some Schema, some QueryAddressableProperty>,
+        defaultValue: (some SQLExpression)? = SQLRaw?.none
+    ) -> Self where Self == SQLLAGExpression {
+        .lag(.column(keypath), offset: .column(offset), defaultValue: defaultValue)
+    }
+
+    /// Allow specifying an argument to the `lag()` SQL function using a Fluent model keypath.
+    static func lag(
+        _ keypath: some SQLExpression,
+        offset: KeyPath<some Schema, some QueryAddressableProperty>,
+        defaultValue: (some SQLExpression)? = SQLRaw?.none
+    ) -> Self where Self == SQLLAGExpression {
+        .lag(.column(keypath), offset: .column(offset), defaultValue: defaultValue)
+    }
+
+    /// Allow specifying an argument to the `lag()` SQL function using a Fluent model keypath.
+    static func lag(
+        _ keypath: KeyPath<some Schema, some QueryAddressableProperty>,
+        offset: some SQLExpression,
+        defaultValue: (some SQLExpression)? = SQLRaw?.none
+    ) -> Self where Self == SQLLAGExpression {
+        .lag(.column(keypath), offset: offset, defaultValue: defaultValue)
+    }
+}
+#endif
