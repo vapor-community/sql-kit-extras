9211d9b794f9533b555baa8b6e6268d8c3e75318
Paul Toffoloni <paultoffoloni@gmail.com>
2025-08-09 11:56:26 +0200

    Use Gwynne's approach

--- Sources/SQLKitExtras/FluentSQLKitExtras/Expressions/SQLCast+FluentKeypaths.swift
+++ Sources/SQLKitExtras/FluentSQLKitExtras/Expressions/SQLCast+FluentKeypaths.swift
@@ -11,5 +11,14 @@ extension SQLExpression {
     ) -> Self where Self == SQLCastExpression {
         .cast(.column(column), to: type)
     }
+
+    /// Convenience method for creating a ``SQLCastExpression`` expression using a Fluent keypath for the value and an expression
+    /// for the desired type.
+    public static func cast(
+        _ column: KeyPath<some Schema, some QueryAddressableProperty>,
+        to type: some SQLExpression
+    ) -> Self where Self == SQLCastExpression {
+        .cast(.column(column), to: type)
+    }
 }
 #endif
--- Sources/SQLKitExtras/SQLKitExtras/Expressions/SQLCastExpression.swift
+++ Sources/SQLKitExtras/SQLKitExtras/Expressions/SQLCastExpression.swift
@@ -6,27 +6,41 @@ public struct SQLCastExpression: SQLExpression {
     public let original: any SQLExpression
 
     /// The desired type to cast the original expression to.
-    public let desiredType: String
+    public let desiredType: any SQLExpression
+
+    /// Create a new ``SQLCastExpression``.
+    ///
+    /// - Parameters:
+    ///   - original: The original expression to be cast.
+    ///   - desiredType: The desired type to cast the original expression to.
+    public init(expr: some SQLExpression, castType: some SQLExpression) {
+        self.original = expr
+        self.desiredType = castType
+    }
 
     /// Convenience initializer for creating a ``SQLCastExpression`` with a string for the original expression and a string for the desired type.
     ///
     /// - Parameters:
     ///   - original: The original expression to be cast.
     ///   - desiredType: The desired type to cast the original expression to, represented as a string.
-    public init(_ original: some SQLExpression, to type: String) {
-        self.original = original
-        self.desiredType = type
+    public init(_ original: some SQLExpression, to type: some StringProtocol) {
+        self.init(expr: original, castType: .identifier(type))
     }
 
     /// See `SQLExpression.serialize(to:)`.
     public func serialize(to serializer: inout SQLSerializer) {
-        if serializer.dialect.name == "mysql" {
-            SQLFunction("CAST", args: SQLAlias(self.original, as: SQLRaw(self.desiredType)))
-                .serialize(to: &serializer)
+        let desiredType: any SQLExpression = if 
+            serializer.dialect.name == "mysql",
+            let ident = self.desiredType as? SQLIdentifier,
+            ident.string.allSatisfy({ $0.isASCII && ($0.isLowercase || $0.isUppercase || $0.isWholeNumber || $0 == "_") })
+         {
+            SQLRaw(ident.string)
         } else {
-            SQLFunction("CAST", args: SQLAlias(self.original, as: SQLIdentifier(self.desiredType)))
-                .serialize(to: &serializer)
+            self.desiredType
         }
+
+        SQLFunction("CAST", args: SQLAlias(self.original, as: desiredType))
+            .serialize(to: &serializer)
     }
 }
 
@@ -34,7 +48,7 @@ extension SQLExpression {
     /// Convenience method for creating a ``SQLCastExpression`` using a column name and a string for the desired type.
     public static func cast(
         _ column: some StringProtocol,
-        to type: String
+        to type: some StringProtocol
     ) -> Self where Self == SQLCastExpression {
         .cast(.column(column), to: type)
     }
@@ -42,8 +56,16 @@ extension SQLExpression {
     /// Convenience method for creating a ``SQLCastExpression`` using a column name and a string for the desired type.
     public static func cast(
         _ column: some SQLExpression,
-        to type: String
+        to type: some StringProtocol
     ) -> Self where Self == SQLCastExpression {
         .init(column, to: type)
+    }
+
+    /// Convenience method for creating a ``SQLCastExpression`` using a column name and an expression for the desired type.
+    public static func cast(
+        _ column: some SQLExpression,
+        to type: some SQLExpression
+    ) -> Self where Self == SQLCastExpression {
+        .init(expr: column, castType: type)
     }
 }
