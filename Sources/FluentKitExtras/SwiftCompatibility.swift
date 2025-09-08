9619f0fe1aff9eb943ca4d25dc1d47b179e9dfa3
Gwynne Raskind <gwynne@vapor.codes>
2025-09-08 04:19:59 -0500

    Fix SendableMetatype handling (#7)
    The `SendableMetatype` thing I did in the previous PR wasn't quite right. This fixes it.

--- Sources/FluentKitExtras/SwiftCompatibility.swift
+++ Sources/FluentKitExtras/SwiftCompatibility.swift
@@ -1,9 +1,18 @@
 import FluentKit
 
 #if compiler(>=6.2)
-@_marker public protocol _SQLKitExtrasSendableMetatype: SendableMetatype {}
+public typealias _SQLKitExtrasSendableMetatype = SendableMetatype
 #else
-@_marker public protocol _SQLKitExtrasSendableMetatype {}
+public typealias _SQLKitExtrasSendableMetatype = Any
 #endif
 
-extension FluentKit.FieldProperty: _SQLKitExtrasSendableMetatype {}
+extension FluentKit.BooleanProperty: _SQLKitExtrasSendableMetatype {}
+extension FluentKit.OptionalBooleanProperty: _SQLKitExtrasSendableMetatype {}
+
+extension FlatGroupProperty: _SQLKitExtrasSendableMetatype {}
+extension OptionalPointerProperty: _SQLKitExtrasSendableMetatype {}
+extension OptionalReferenceProperty: _SQLKitExtrasSendableMetatype {}
+extension PointerProperty: _SQLKitExtrasSendableMetatype {}
+extension ReferencesProperty: _SQLKitExtrasSendableMetatype {}
+extension RequiredTimestampProperty: _SQLKitExtrasSendableMetatype {}
+
