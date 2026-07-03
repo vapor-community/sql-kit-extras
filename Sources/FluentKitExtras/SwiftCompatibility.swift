b0c943db5675fb897e3d7d39a36be5f7b7ad1420
Gwynne Raskind <gwynne@vapor.codes>
2026-07-03 17:43:38 -0500

    Deprecate the old SendableMetatype alias instead of removing it

--- Sources/FluentKitExtras/SwiftCompatibility.swift
+++ Sources/FluentKitExtras/SwiftCompatibility.swift
@@ -1,4 +1,7 @@
 import FluentKit
 
+@available(*, deprecated, renamed: "SendableMetatype", message: "Use SendableMetatype instead.")
+public typealias _SQLKitExtrasSendableMetatype = SendableMetatype
+
 extension BooleanProperty: @retroactive SendableMetatype {}
 extension OptionalBooleanProperty: @retroactive SendableMetatype {}
